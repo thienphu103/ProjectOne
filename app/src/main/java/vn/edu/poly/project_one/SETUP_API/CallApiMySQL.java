d761c2bac07a6d0a16c7ef0ebd08a80ee761445a
thienphu <thienphu103@gmail.com>
2017-11-28 21:39:18 +0700

    add login google and facebook

---
+++ app/src/main/java/vn/edu/poly/project_one/SETUP_API/CallApiMySQL.java
@@ -0,0 +1,98 @@
+package vn.edu.poly.project_one.SETUP_API;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.util.Base64;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by A on 11/28/2017.
+ */
+
+public class CallApiMySQL extends Activity {
+    public static final String URL_CALL_API_GET_DATA = "";
+    public static final String URL_CALL_API_UP_DATA = "";
+
+
+    public void upData() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_CALL_API_UP_DATA, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "" + response.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "" + error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> stringMap = new HashMap<>();
+//                Bitmap bitmap = ((BitmapDrawable)imageViewAvatar.getDrawable()).getBitmap();
+//                Bitmap image_fb = BitmapFactory.decodeStream(url_fb.openConnection().getInputStream());
+//                ss = decodeImage(image_fb);
+//                    stringMap.put("image_name", IdUser);
+//                    stringMap.put("image_fiel", s);
+                return stringMap;
+            }
+
+        };
+        requestQueue.add(stringRequest);
+    }
+
+
+
+    private void getData() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, URL_CALL_API_GET_DATA, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject object = response.getJSONObject(i);
+//                        id = object.getInt("id_image");
+//                        name = object.getString("path_image");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "" + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        requestQueue.add(arrayRequest);
+    }
+    public String decodeImage(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        byte[] byteImage = outputStream.toByteArray();
+        String encodeImage = Base64.encodeToString(byteImage, Base64.DEFAULT);
+        return encodeImage;
+    }
+}
